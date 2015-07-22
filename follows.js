0cecfaeaf6ff8e5e4dee777e8129f539773606d4
willlawler <will.d.lawler@gmail.com>
2015-07-22 16:50:15 +1000

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,10 +12,10 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'tySu8vkGNjECtJz77M5JQJQtLhozqTQAoXur2637nHGIUxMq', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [570476090], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Will's Home, // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
